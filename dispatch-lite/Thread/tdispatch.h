c581de322ec3923488b1689dfd325eef5656b023
jasenhuang <scut.xusheng@gmail.com>
2015-07-30 20:50:48 +0800

    add project

---
+++ dispatch-lite/Thread/tdispatch.h
@@ -0,0 +1,77 @@
+//
+//  tdispatch.h
+//  lite
+//
+//  Created by jasenhuang on 15/7/22.
+//  Copyright (c) 2015年 tencent. All rights reserved.
+//
+
+#ifndef __lite__tdispatch__
+#define __lite__tdispatch__
+
+#include "tthread.h"
+
+#ifdef USE_NAMESPACE
+namespace Lite {
+#endif
+
+#ifdef USE_NAMESPACE
+    
+#define dispatch_logic_async(x) Lite::Dispatch::PostTask(Lite::Dispatch::LOGIC, x)
+#define dispatch_io_async(x) Lite::Dispatch::PostTask(Lite::Dispatch::IO, x)
+#define dispatch_after(milisecond, x) Lite::MessageLoop::current()->PostDelayedTask(x, Lite::TimeDelta::FromMilliseconds(milisecond))
+    
+#else
+    
+#define dispatch_logic_async(x) Dispatch::PostTask(Dispatch::LOGIC, x)
+#define dispatch_io_async(x) Dispatch::PostTask(Dispatch::IO, x)
+#define dispatch_after(milisecond, x) MessageLoop::current()->PostDelayedTask(x, TimeDelta::FromMilliseconds(milisecond))
+    
+#endif
+
+class Dispatch {
+public:
+    
+    enum ID {
+        // The main thread.
+        UI = 0,
+        // This is the thread that interacts with the io.
+        IO,
+        // This is the thread that interacts with the db.
+        DB,
+        // This is the thread that process GY logic.
+        LOGIC,
+        // count
+        ID_COUNT
+    };
+    
+    static void Init();
+    static void Stop();
+
+    static Thread* CreateThread(const char* name);
+    
+    static bool PostTask(ID identifier, const std::function<void()>& task);
+    static bool PostDelayedTask(ID identifier, const std::function<void()>& task, TimeDelta delay);
+    static bool PostNonNestableTask(ID identifier, const std::function<void()>& task);
+    static bool PostNonNestableDelayedTask(ID identifier, const std::function<void()>& task, TimeDelta delay);
+    
+    static bool PostTask(Thread* thread, const std::function<void()>& task);
+    static bool PostDelayedTask(Thread* thread, const std::function<void()>& task, TimeDelta delay);
+    static bool PostNonNestableTask(Thread* thread, const std::function<void()>& task);
+    static bool PostNonNestableDelayedTask(Thread* thread, const std::function<void()>& task, TimeDelta delay);
+    
+private:
+    
+    static Thread* CreateThread(const char* name, ID identifier);
+    
+    static bool PostTaskHelper(ID identifier, const std::function<void()>& task, TimeDelta delay, bool nestable);
+    static bool PostTaskHelper(Thread* thread, const std::function<void()>& task, TimeDelta delay, bool nestable);
+    
+    DISALLOW_COPY_AND_ASSIGN(Dispatch);
+};
+
+#ifdef USE_NAMESPACE
+};
+#endif
+
+#endif /* defined(dispatchlitetdispatch__) */
---
+++ dispatch-liteTests/testdispatch.mm
@@ -0,0 +1,62 @@
+//
+//  testdispatch.m
+//  dispatchlite
+//
+//  Created by jasenhuang on 15/7/30.
+//  Copyright (c) 2015年 tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#include "tdispatch.h"
+
+#define NeedsToWaitForBlock() __block BOOL blockFinished = NO
+#define BlockFinished() blockFinished = YES
+#define WaitForBlock() while (CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true) && !blockFinished)
+
+@interface testdispatch : XCTestCase
+
+@end
+
+@implementation testdispatch
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+    Lite::Dispatch::Init();
+    NeedsToWaitForBlock();
+    for (NSInteger i =0 ; i < 10; ++i){
+        dispatch_logic_async([=]{
+            NSLog(@"hello world [%@]", @(i));
+            dispatch_after(1000 * (10 - i) , [=]{
+                NSLog(@"hello world [%@]", @(i));
+            });
+        });
+        Lite::Dispatch::PostTask(Lite::Dispatch::LOGIC, [=]{
+            NSLog(@"hello world [%@]", @(i));
+        });
+        Lite::Dispatch::PostDelayedTask(Lite::Dispatch::LOGIC, [=]{
+            NSLog(@"hello world [%@]", @(i));
+        }, Lite::TimeDelta::FromSeconds(10 - i));
+    }
+    WaitForBlock();
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
